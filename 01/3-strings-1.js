725926806ca7069868058388c194691f3dc61d3b
Pavel Nechaev <pavel.netchaev@gmail.com>
2019-08-12 00:47:39 +0300

    Solved tasks from 1st homework

---
+++ 01/3-strings-1.js
@@ -0,0 +1,13 @@
+/*
+Сделать первый символ заглавным
+
+Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом. Например:
+
+ucFirst("вася") == "Вася";
+*/
+
+function ucFirst(str){
+    return (!str) ? str[0].toUpperCase() + str.substr(1) : '';
+}
+
+alert( ucFirst('вася') );
---
+++ 01/3-strings-2.js
@@ -0,0 +1,15 @@
+/*
+Проверка на спам
+
+Напишите функцию checkSpam(str), возвращающую true, если str содержит 'viagra' или 'XXX', а иначе false.
+
+Функция должна быть нечувствительна к регистру:
+
+checkSpam('buy ViAgRA now') == true
+checkSpam('free xxxxx') == true
+checkSpam("innocent rabbit") == false
+*/
+
+function checkSpam(str){
+    return ( str.toLowerCase().includes('viagra') || str.toLowerCase().includes('xxx') ) ? true : false;
+}
